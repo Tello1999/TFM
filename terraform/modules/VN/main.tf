5d6da86e6ea3bd21153503478f02db1a628d9422
jaimetello <jtellos@alumnos.nebrija.es>
2024-12-08 20:48:18 +0100

    TerraformDemo1.5

--- terraform/modules/VN/main.tf
+++ terraform/modules/VN/main.tf
@@ -4,9 +4,4 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = "github-action"
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  subnet {
-    name             = "subnet1"
-    address_prefixes = ["10.0.1.0/24"]
-  }
 }
