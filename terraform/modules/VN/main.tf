de54bf2af87bb9c6a2c3976e4e674eb8610311a5
jaimetello <jtellos@alumnos.nebrija.es>
2024-12-08 20:11:58 +0100

    TerraformDemo1.5

--- terraform/modules/VN/main.tf
+++ terraform/modules/VN/main.tf
@@ -3,4 +3,5 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name      = var.rgname
   location                 = var.location
   address_space            = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 }
