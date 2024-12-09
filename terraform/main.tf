84e7a4d3fea4ab7cfb2b2e013d8028a63cfba33b
Juan <juan.santos584@comunidadunir.net>
2024-12-10 00:00:58 +0100

    Ejecucion de: a4b924a2-af92-4eb6-b806-f274986c3ea5

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,17 @@ terraform {
     resource_group_name  = "TerraformDemo"
     storage_account_name = "strterraform"
     container_name       = "tfstatefile"
-    key                  = "dev.terraform@@uuid@@.tfstate"
+    key                  = "dev.terraforma4b924a2-af92-4eb6-b806-f274986c3ea5.tfstate"
   }
+}
+module "RG1" {
+  source   = "./modules/RG"
+  rgname   = "perez"
+  location = "East US"
+}
+module "SA1" {
+  source   = "./modules/StorageAccount"
+  sname    = "xyzyk"
+  rgname   = "github-action"
+  location = "East US"
 }
