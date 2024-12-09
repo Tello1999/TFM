bd18e3c4b6d4afe5e59079f610983338625a8122
Juan <juan.santos584@comunidadunir.net>
2024-12-09 22:57:06 +0100

    reorganizar templates

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "TerraformDemo"
     storage_account_name = "strterraform"
     container_name       = "tfstatefile"
-    key                  = "dev.terraform@@uuid@@.tfstate"
+    key                  = "dev.terraform.@@uuid@@.tfstate"
   }
 }
