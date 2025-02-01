c17fbc17fb35eede91942f6fe3ba70ca65473f23
Juan <juan.santos584@comunidadunir.net>
2025-02-01 18:45:14 +0100

    Ejecucion de: 21559e3e-0db8-4b6d-a6ec-1a2043ddf0f4

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,17 @@ terraform {
     resource_group_name  = "TerraformDemo"
     storage_account_name = "strterraform"
     container_name       = "tfstatefile"
-    key                  = "dev.terraform@@uuid@@.tfstate"
+    key                  = "dev.terraform21559e3e-0db8-4b6d-a6ec-1a2043ddf0f4.tfstate"
   }
+}
+module "RG1" {
+  source   = "./modules/RG"
+  rgname   = "RgTFM"
+  location = "East US"
+}
+module "SA1" {
+  source   = "./modules/StorageAccount"
+  sname    = "SaTFM"
+  rgname   = "github-action"
+  location = "East US"
 }
