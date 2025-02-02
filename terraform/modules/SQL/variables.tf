317b95f112f7c2ec37dfcd4dfbd8ceb38859d872
Tello1999 <jaimetellosanchez@gmail.com>
2025-02-02 12:16:31 +0100

    Create variables.tf

---
+++ terraform/modules/SQL/variables.tf
@@ -0,0 +1,18 @@
+variable "servername" {
+  type = string
+}
+variable "serverrgname" {
+  type = string
+}
+variable "serverlocation" {
+  type = string
+}
+variable "adminlogin" {
+  type = string
+}
+variable "loginpass" {
+  type = string
+}
+variable "dbname" {
+  type = string
+}
