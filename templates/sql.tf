9a20c6dd250844423f9bb8b04cc1f2b1cfea5af8
JuanSantosCuerva2000 <juan.santos584@comunidadunir.net>
2025-03-09 18:32:29 +0100

    Update sql.tf

--- templates/sql.tf
+++ templates/sql.tf
@@ -1,9 +1,9 @@
 module "SQLSERVER" {
-  source = "./modules/SQL"
-  servername = "@@servername@@"
-  serverrgname = "@@serverrgname@@"
-  serverlocation = "@@serverlocation@@"
-  administrator_login = "@@administrator_login@@"
+  source                       = "./modules/SQL"
+  servername                   = "@@servername@@"
+  serverrgname                 = "@@serverrgname@@"
+  serverlocation               = "@@serverlocation@@"
+  administrator_login          = "@@administrator_login@@"
   administrator_login_password = "@@administrator_login_password@@."
 }
 module "dbsql" {
