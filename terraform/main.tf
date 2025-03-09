56972a61d6049b7255777969c8c8621a4b622357
Juan <juan.santos584@comunidadunir.net>
2025-03-09 19:53:20 +0100

    Ejecucion de: 6d449675-2741-432b-883d-4d80aa6aabde

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,21 @@ terraform {
     resource_group_name  = "TFM"
     storage_account_name = "terraformtfm"
     container_name       = "tfstatefile"
-    key                  = "dev.terraform@@uuid@@.tfstate"
+    key                  = "dev.terraform6d449675-2741-432b-883d-4d80aa6aabde.tfstate"
   }
 }
+module "RG1" {
+  source   = "./modules/RG"
+  rgname   = "test"
+  location = "East US"
+}
+module "SQLSERVER" {
+  source         = "./modules/SQL"
+  servername     = "unir"
+  serverrgname   = "github-action"
+  serverlocation = "East US"
+  adminlogin     = "master"
+  loginpass      = "4-v3ry-53cr37-p455w0rd."
+  dbname         = "unir2"
+}
+
