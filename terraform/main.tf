397a799f0835e53d7fd4fe23e701147ca23590bf
JuanSantosCuerva2000 <juan.santos584@comunidadunir.net>
2025-03-09 18:09:18 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "TFM"
-    storage_account_name = "abc"
+    storage_account_name = "terraformTFM"
     container_name       = "tfstatefile"
     key                  = "dev.terraform@@uuid@@.tfstate"
   }
