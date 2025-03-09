ca489c9d43cd9706c06c36a7c68c629ca4fc0cff
JuanSantosCuerva2000 <juan.santos584@comunidadunir.net>
2025-03-09 18:58:32 +0100

    Update main.tf

--- terraform/modules/SQL/main.tf
+++ terraform/modules/SQL/main.tf
@@ -13,7 +13,6 @@ resource "azurerm_mssql_database" "exampledb" {
   license_type = "LicenseIncluded"
   max_size_gb  = 2
   sku_name     = "S0"
-  enclave_type = "VBS"
 
   tags = {
     foo = "bar"
